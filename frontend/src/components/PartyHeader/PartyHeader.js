1f6d92f5addd9a8b3aa068ff74467b75fcac1012
logansw <logansw@uw.edu>
2022-02-19 23:35:16 -0800

    Fixed PartyHeader so it shows details for all group sizes

--- frontend/src/components/PartyHeader/PartyHeader.js
+++ frontend/src/components/PartyHeader/PartyHeader.js
@@ -31,96 +31,74 @@ export const PartyHeader = ({ setIsEditing, setPinsOpen }) => {
     const members = Object.values(channel.state.members).filter(
       ({ user }) => user.id !== client.userID,
     );
-    const additionalMembers = members.length - 3;
-
-    if (!members.length) {
-      return (
-        <div className='party-header__name-wrapper'>
-          <Avatar image={null} size={50} />
-          <Button onClick={() => handleShow('title')} className='party-header__title'>
-            <p className='party-header__name user'>{nameInput}</p>
-          </Button>
-          <div className='party-header__grow' />
-          <Button onClick={() => handleShow('up next')} className='party-header__details'>
-            <p className='party-header__regular'> Up Next:</p>
-            <p className='party-header__bold'>{scheduleInput}</p>
-          </Button>
-          <Button onClick={() => handleShow('currently watching')} className='party-header__details'>
-            <p className='party-header__regular'>Currently Watching:</p>
-            <p className='party-header__bold'>{watchInput}</p>
-          </Button>
-          <Button variant="danger" className='party-header__button'>Invite Friends</Button>
-
-          <Modal show={show === 'title'} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Edit Group Name</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <input type='text' value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Cancel
-              </Button>
-              <Button variant="danger" onClick={handleClose}>
-                Save
-              </Button>
-            </Modal.Footer>
-          </Modal>
-
-          <Modal show={show === 'up next'} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Up Next</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <input type='text' value={scheduleInput} onChange={(e) => setScheduleInput(e.target.value)} />
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Cancel
-              </Button>
-              <Button variant="danger" onClick={handleClose}>
-                Save
-              </Button>
-            </Modal.Footer>
-          </Modal>
-
-          <Modal show={show === 'currently watching'} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Currently Watching</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <input type='text' value={watchInput} onChange={(e) => setWatchInput(e.target.value)} />
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Cancel
-              </Button>
-              <Button variant="danger" onClick={handleClose}>
-                Save
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
-      );
-    }
 
     return (
       <div className='party-header__name-wrapper'>
-        {members.map(({ user }, i) => {
-          if (i > 2) return null;
-          return (
-            <div key={i} className='party-header__name-multi'>
-              <Avatar image={user.image} name={user.name || user.id} size={32} />
-              <p className='party-header__name user'>
-                {user.name || user.id || 'Johnny Blaze'}
-              </p>
-            </div>
-          );
-        })}
-        {additionalMembers > 0 && (
-          <p className='party-header__name user'>{`and ${additionalMembers} more`}</p>
-        )}
+        <Avatar image={null} size={50} />
+        <Button onClick={() => handleShow('title')} className='party-header__title'>
+          <p className='party-header__name user'>{nameInput}</p>
+        </Button>
+        <div className='party-header__grow' />
+        <Button onClick={() => handleShow('up next')} className='party-header__details'>
+          <p className='party-header__regular'> Up Next:</p>
+          <p className='party-header__bold'>{scheduleInput}</p>
+        </Button>
+        <Button onClick={() => handleShow('currently watching')} className='party-header__details'>
+          <p className='party-header__regular'>Currently Watching:</p>
+          <p className='party-header__bold'>{watchInput}</p>
+        </Button>
+        <Button variant="danger" className='party-header__button'>Invite Friends</Button>
+
+        <Modal show={show === 'title'} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Group Name</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <input type='text' value={nameInput} onChange={(e) => setNameInput(e.target.value)} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={handleClose}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={show === 'up next'} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Up Next</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <input type='text' value={scheduleInput} onChange={(e) => setScheduleInput(e.target.value)} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={handleClose}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={show === 'currently watching'} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Currently Watching</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <input type='text' value={watchInput} onChange={(e) => setWatchInput(e.target.value)} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={handleClose}>
+              Save
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   };
