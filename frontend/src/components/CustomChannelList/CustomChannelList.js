9ef0160be59033c0de8781428a642cca4ced4a0b
logansw <logansw@uw.edu>
2022-03-06 14:26:01 -0800

    Commented CustomChannelList and CustomChannelPreview

--- frontend/src/components/CustomChannelList/CustomChannelList.js
+++ frontend/src/components/CustomChannelList/CustomChannelList.js
@@ -1,33 +1,16 @@
-import React, { useState } from 'react';
-import {
-  Avatar,
-  useChannelActionContext,
-  useChannelStateContext,
-  useChatContext,
-} from 'stream-chat-react';
+import React from 'react';
 
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Modal from 'react-bootstrap/Modal';
-import axios from 'axios';
-import Cookies from 'universal-cookie';
-import {Link, Navigate} from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import './CustomChannelList.css';
 
-const shiba = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1AmDqL2HOj7N95ONvLGeG4zm1S82PRheMsA&usqp=CAU';
-const cookies = new Cookies();
+/*
+  CUSTOM CHANNEL LIST
 
-export const CustomChannelList = ({ children, error = false, loading, onCreateChannel }) => {
-  const { client, setActiveChannel } = useChatContext();
-  const { id, name = 'Example User' } = client.user || {};
-
-  const switchCreateGroup = () => {
-    <Navigate to="/createGroup"/>
-  }
-
-
-
+  Side bar displaying all of a user's watch parties.
+*/
+export const CustomChannelList = ({ children, error = false }) => {
   const ListHeaderWrapper = ({ children }) => {
     return (
       <div className='messaging__channel-list'>
@@ -55,7 +38,6 @@ export const CustomChannelList = ({ children, error = false, loading, onCreateCh
     );
   }
 
-
   if (!children.props.children) {
     return (
       <ListHeaderWrapper>
@@ -70,5 +52,5 @@ export const CustomChannelList = ({ children, error = false, loading, onCreateCh
     <ListHeaderWrapper>
       {children}
     </ListHeaderWrapper>
-    );
+  );
 };
--- frontend/src/components/CustomChannelPreview/CustomChannelPreview.js
+++ frontend/src/components/CustomChannelPreview/CustomChannelPreview.js
@@ -3,17 +3,19 @@ import { Avatar, ChatContext } from 'stream-chat-react';
 
 import './CustomChannelPreview.css';
 
+// Placeholder group picture
 const shiba = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1AmDqL2HOj7N95ONvLGeG4zm1S82PRheMsA&usqp=CAU';
 
+/*
+  CUSTOM CHANNEL PREVIEW
 
+  Card for a single Watch Party. Displays the group's picture, name, current show,
+  and next Watch By date.
+*/
 export const CustomChannelPreview = (props) => {
-  const { channel, latestMessage, setActiveChannel, setIsCreating } = props;
+  const { channel, setActiveChannel, setIsCreating } = props;
 
   const { channel: activeChannel, client } = useContext(ChatContext);
-
-  const members = Object.values(channel.state.members).filter(
-    ({ user }) => user.id !== client.userID,
-  );
 
   return (
     <div
@@ -27,21 +29,15 @@ export const CustomChannelPreview = (props) => {
         setActiveChannel(channel);
       }}
     >
-      <Avatar
-            image={shiba}
-            name={'shiba!'}
-            size={60}
-          />
+      <Avatar image={shiba} name={'shiba!'} size={60} />
       <div className='channel-preview__content-wrapper'>
         <div className='channel-preview__content-top'>
           <p className='channel-preview__content-name'>
             {channel.data.name}
           </p>
         </div>
-        {/* <p className='channel-preview__content-message'>{latestMessage.props.source || 'Send a message'}</p> */}
         <p className='channel-preview__content-message'>{channel.data.show}</p>
         <p className='channel-preview__content-message'>{channel.data.episode} by {channel.data.date}</p>
-
       </div>
     </div>
   );
